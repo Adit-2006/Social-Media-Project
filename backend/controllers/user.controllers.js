5e8f26e00b6bd7116c27e57ccf37239ca9c2e494
Adit T Krishnadas <adittkrishnadas13@gmail.com>
2026-08-25 11:11:32 +0530

    Created login users function and generated web tokend=s using jwt

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js"
 import bcrypt from 'bcrypt' 
+import genToken from "../utils/genToken.js"
 
 export const registerUser = async(req, res) => {
     const {name, username, email, password} = req.body
@@ -12,9 +13,9 @@ export const registerUser = async(req, res) => {
         }
 
         // is username repeating?
-        const userNameExists = await User.findOne({username})
+        const user = await User.findOne({username})
 
-        if (userNameExists){
+        if (user){
             return res.status(400).json({message: "Username already exists"});
         }
 
@@ -30,15 +31,45 @@ export const registerUser = async(req, res) => {
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = bcrypt.hashSync(password, salt);
 
-
-
+        //Generate genToken
         const newUser = await User.create({name, username, email, password : hashedPassword})
+
+        const token = genToken(newUser._id)
+
+        console.log(token);
 
         res.status(201).json(newUser)
 
     }
     catch(err) {
         console.error(err)
-        res.status(500).json({message: "Unable to register user."})
+        res.status(500).json({message: "Unable to register user." + err})
+    }
+}
+
+export const loginUser = async(req, res) => {
+    // login the user
+    try {
+        const {email, password} = req.body
+        if (!email || !password){
+            return res.status(422).json({message: "Email or password is missing"})
+        }
+        const userExists = await User.findOne({email})
+
+        if (!userExists){
+            return res.status(404).json({message: "User not found"})
+        }
+        const correctPassword = bcrypt.compareSync(password, userExists.password)
+
+        if (!correctPassword){
+            return res.status(401).json({message: "Invalid password"})
+        }
+
+        res.status(200).json({message: "Login Successful",
+            user: userExists
+        })
+    }
+    catch(err){
+        res.status(500).json({message: err + " occured"})
     }
 }
