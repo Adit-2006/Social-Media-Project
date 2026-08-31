a3880ad71764c78564fd39a10c636fb00b4ff4da
Adit-2006 <adittkrishnadas13@gmail.com>
2026-08-31 12:19:25 +0530

    Uploading the basic login and signup page to be continued.

--- frontend/social-media/pages/SignUp.jsx
+++ frontend/social-media/pages/SignUp.jsx
@@ -18,10 +18,12 @@ function SignUp() {
     setLoader(true)
     try {
       await axiosInstance.post('/users/register', form)
+      console.log("User registered")
       
     }
-    catch (error) {
-      console.log(error)
+    catch(error) {
+      setErr(error)
+      console.log(err)
     }
   }
 
@@ -37,19 +39,27 @@ function SignUp() {
         <form>
           <div className="mb-4">
             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">Name</label>
-            <input className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" type="text" id="name" placeholder="Full Name" />
+            <input
+              onChange={handleChange}
+              className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" type="text" id="name" placeholder="Full Name" />
           </div>
           <div className="mb-4">
             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">Username</label>
-            <input className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" type="text" id="username" placeholder="Username" />
+            <input
+              onChange={handleChange}
+              className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" type="text" id="username" placeholder="Username" />
           </div>
           <div className="mb-4">
             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">Email</label>
-            <input className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" type="email" id="email" placeholder="you@example.com" />
+            <input
+              onChange={handleChange}
+              className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" type="email" id="email" placeholder="you@example.com" />
           </div>
           <div className="mb-6">
             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">Password</label>
-            <input className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" type="password" id="password" placeholder="********" />
+            <input
+              onCanPlay={handleChange}
+              className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" type="password" id="password" placeholder="********" />
           </div>
           <button className="w-full bg-green-500 text-white py-2 rounded-lg hover:bg-green-600 transition duration-300">Sign Up</button>
         </form>
